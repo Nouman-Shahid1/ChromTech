108de5dbf219598c98c966ae96328d4458b643ff
Nouman-Shahid1 <muhammadnoumansha140@gmail.com>
2024-11-08 01:29:20 +0500

    category and product api added and authentization updated

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const cors = require("cors");
 require("dotenv").config();
@@ -14,7 +16,8 @@ connectDB();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
 app.use(errorHandler);
 
 module.exports = app;
